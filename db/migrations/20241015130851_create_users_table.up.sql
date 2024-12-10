7bb6ca944a8579d3803cd56728d9aac3e68b2cdc
sherwin-77 <sherwinak1@gmail.com>
2024-12-10 19:58:10 +0700

    refactor(migration): Change migration constraint

--- db/migrations/20241015130851_create_users_table.up.sql
+++ db/migrations/20241015130851_create_users_table.up.sql
@@ -1,7 +1,7 @@
 BEGIN;
 
 CREATE TABLE users (
-    id UUID PRIMARY KEY NOT NULL,
+    id UUID PRIMARY KEY,
     username VARCHAR(255) NOT NULL,
     email VARCHAR(255) NOT NULL,
     password VARCHAR(255) NOT NULL,
